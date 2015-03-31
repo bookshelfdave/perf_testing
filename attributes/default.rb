9fd54f47b60b2cbdcae66006f9798adc182a0b64
Dave Parfitt <dparfitt@chef.io>
2015-03-31 17:15:50 -0400

    fixed url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
+node.default["collectd"]["url"] = "https://s3.amazonaws.com/collectd-5.4.1/collectd-5.4.1.tar.gz"
 node.default['collectd']['python_plugins'] = {}
 node.default['collectd']['version'] = "5.4.1"
-node.default['collectd']['checksum'] = "6514ab3f7fd2135d2713f1ab25068841"
+#node.default['collectd']['checksum'] = "6514ab3f7fd2135d2713f1ab25068841"
 node.default['plugins'] = {
     "syslog" => {
       "config" => { "LogLevel" => "Info" }
