46f589c2705a132cbff19dfe205abc1e1f7510dd
Dave Parfitt <dparfitt@chef.io>
2015-03-31 17:12:53 -0400

    wip

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 node.default['collectd']['python_plugins'] = {}
 node.default['collectd']['version'] = "5.4.1"
+node.default['collectd']['checksum'] = "6514ab3f7fd2135d2713f1ab25068841"
 node.default['plugins'] = {
     "syslog" => {
       "config" => { "LogLevel" => "Info" }
